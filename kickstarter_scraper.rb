685218c847f88a17b4e3bfa01de3db57a5217a4f
dwane richards <dwanerich@gmail.com>
2020-02-04 01:07:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,10 +2,13 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
+  new_hash = {}
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
+
+
 
 create_project_hash
 # projects: kickstarter.css("li.project.gird_4")
